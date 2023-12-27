d891277d5837368ca207327d0be40670c0614640
Paska <43591633+exzettabyte@users.noreply.github.com>
2023-12-27 21:13:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysql://pwlresponsi_belongbite:a33f82fe60054555fd463fcc0a724f4138cb2f33@zp6.h.filess.io:3307/pwlresponsi_belongbite'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://pwlresponsi_belongbite:a33f82fe60054555fd463fcc0a724f4138cb2f33@zp6.h.filess.io:3307/pwlresponsi_belongbite'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
